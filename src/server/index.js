d473b4f7563aecb25ac5cc8933281d0e8c5343e8
vrykolakas166 <hhtb.bdh@gmail.com>
2023-10-31 15:02:52 +0700

    update for deploying

--- src/server/index.js
+++ src/server/index.js
@@ -22,8 +22,8 @@ app.use(
 app.use(express.static("dist"));
 
 app.get("/", function (req, res) {
-  //res.sendFile("dist/index.html");
-  res.sendFile(path.resolve("src/client/views/index.html"));
+  res.sendFile("dist/index.html");
+  //res.sendFile(path.resolve("src/client/views/index.html"));
 });
 
 app.get("/getApiKey", function (req, res) {
